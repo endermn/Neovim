d7596abd03e4be3857b9b59fe5d6ab44214758c9
Winner <winner@Winners-MacBook-Pro.local>
2024-10-03 09:29:59 +0300

    mulitple cursors

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -4,6 +4,28 @@ local exist, user_config = pcall(require, "user.user_config")
 local group = exist and type(user_config) == "table" and user_config.enable_plugins or {}
 local plugins = exist and type(user_config) == "table" and user_config.plugins or {}
 require("lazy").setup({
+    {
+      "brenton-leighton/multiple-cursors.nvim",
+      version = "*",  -- Use the latest tagged version
+      opts = {},  -- This causes the plugin setup function to be called
+      keys = {
+        {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "x"}, desc = "Add cursor and move down"},
+        {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "x"}, desc = "Add cursor and move up"},
+
+        {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move up"},
+        {"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move down"},
+
+        {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove cursor"},
+
+        {"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
+        {"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
+
+        {"<Leader>d", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
+        {"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
+
+        {"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
+      },
+    },
 	{
 		"stevearc/aerial.nvim",
 		cond = enabled(group, "aerial"),
--- lua/plugin-configs/lsp.lua
+++ lua/plugin-configs/lsp.lua
@@ -42,3 +42,34 @@ lspconfig.gopls.setup {
   capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 }
 
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    -- Your custom on_attach function here, if needed
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = vim.api.nvim_create_augroup("ClangdFormat", {}),
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { async = false }
+          end,
+        })
+    end
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+}
+
+lspconfig.html.setup({
+  on_attach = function(client, bufnr)
+    -- Custom keybindings or features can be set here
+    -- Example for keymaps
+    local opts = { noremap=true, silent=true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  filetypes = { "html", "htmldjango" },  -- You can add more filetypes if needed
+})
+
+
+
